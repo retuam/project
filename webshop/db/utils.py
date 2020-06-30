777c5848968ee0695b72ac25a378c543b8b5c116
kirillkravcenko <retuam@gmail.com>
2020-06-30 21:19:44 +0300

    3

--- webshop/db/utils.py
+++ webshop/db/utils.py
@@ -12,5 +12,45 @@ def init_texts():
     )
 
 
+def init_texts_next():
+    Text.objects.create(
+        title=Text.TITLES['to_cart'],
+        body='Вы добавляете товар в корзину'
+    )
+    Text.objects.create(
+        title=Text.TITLES['from_cart'],
+        body='Вы перешли в корзину'
+    )
+    Text.objects.create(
+        title=Text.TITLES['category'],
+        body='Вы выбрали категорию'
+    )
+    Text.objects.create(
+        title=Text.TITLES['add'],
+        body='Вы добавили товар в корзину'
+    )
+    Text.objects.create(
+        title=Text.TITLES['erase'],
+        body='Вы очистили корзину'
+    )
+    Text.objects.create(
+        title=Text.TITLES['checkout'],
+        body='Вы оплатили товары'
+    )
+    Text.objects.create(
+        title=Text.TITLES['empty'],
+        body='Ваша корзина пуста'
+    )
+    Text.objects.create(
+        title=Text.TITLES['discount'],
+        body='Вы выбрали товары со скидкой'
+    )
+    Text.objects.create(
+        title=Text.TITLES['finish'],
+        body='Вы завершаете покупку'
+    )
+
+
 if __name__ == '__main__':
-    init_texts()
+    # init_texts()
+    init_texts_next()
