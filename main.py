6bf4cd7d845b1aa3b29919982b841c783e579256
kirillkravcenko <retuam@gmail.com>
2020-07-04 23:02:14 +0300

    final before testing

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ def webhook():
 if __name__ == '__main__':
     # bot.polling()
     import time
-    # bot.remove_webhook()
-    # time.sleep(1)
-    # bot.set_webhook(
-    #     config.WEBHOOK_URL,
-    #     certificate=open('webhook_cert.pem', 'r')
-    # )
+    bot.remove_webhook()
+    time.sleep(1)
+    bot.set_webhook(
+        config.WEBHOOK_URL,
+        certificate=open('webhook_cert.pem', 'r')
+    )
     app.register_blueprint(api_bp)
     app.run(debug=True)
 
