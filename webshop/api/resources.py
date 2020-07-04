c93827d35aff9cddbb94f08287c2a029cf0edd7f
kirillkravcenko <retuam@gmail.com>
2020-07-04 14:40:06 +0300

    final before testing

--- webshop/api/resources.py
+++ webshop/api/resources.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from schemas import *
-from .db.models import *
+from project.webshop.db.models import *
 from flask import request, jsonify
 import json
 from marshmallow import ValidationError
