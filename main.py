e877d32978104b8137182c885b274d35ac8801ff
kirillkravcenko <retuam@gmail.com>
2020-07-04 15:33:02 +0300

    final before testing

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ app = Flask(__name__)
 app.register_blueprint(api_bp)
 
 
-# @app.route(config.WEBHOOK_PATH, methods=['POST'])
-# def webhook():
-#     if request.headers.get('content-type') == 'application/json':
-#         json_string = request.get_data().decode('utf-8')
-#         update = Update.de_json(json_string)
-#         bot.process_new_updates([update])
-#         return ''
-#     else:
-#         abort(403)
+@app.route(config.WEBHOOK_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
 
 
 if __name__ == '__main__':
     # bot.polling()
-    # import time
-    # bot.remove_webhook()
-    # time.sleep(1)
+    import time
+    bot.remove_webhook()
+    time.sleep(1)
     # bot.set_webhook(
     #     config.WEBHOOK_URL,
     #     certificate=open('webhook_cert.pem', 'r')
