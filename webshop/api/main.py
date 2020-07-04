28a9b13fd6b1c97a36c69e4a7520ddba3ea155ba
kirillkravcenko <retuam@gmail.com>
2020-07-04 15:32:54 +0300

    final before testing

--- webshop/api/main.py
+++ webshop/api/main.py
@@ -3,11 +3,11 @@ from flask_restful import Api
 from resources import *
 
 
-# api_bp = Blueprint('api', __name__)
-# api = Api(api_bp)
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
 
-app = Flask(__name__)
-api = Api(app)
+# app = Flask(__name__)
+# api = Api(app)
 
 api.add_resource(
     TotalResource,
