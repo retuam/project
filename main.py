f44e146851d5afffd3bbd9463dc5320aba6b77b2
kirillkravcenko <retuam@gmail.com>
2020-07-04 21:23:41 +0300

    final before testing

--- main.py
+++ main.py
@@ -9,24 +9,36 @@ app = Flask(__name__)
 # app.register_blueprint(api_bp)
 
 
-@app.route(config.WEBHOOK_PATH, methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
+@app.route('/', methods=['GET'])
+def index():
+    print(1)
+    return 2
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    print(10)
+    return 20
+
+
+# @app.route(config.WEBHOOK_PATH, methods=['POST'])
+# def webhook():
+#     if request.headers.get('content-type') == 'application/json':
+#         json_string = request.get_data().decode('utf-8')
+#         update = Update.de_json(json_string)
+#         bot.process_new_updates([update])
+#         return ''
+#     else:
+#         abort(403)
 
 
 if __name__ == '__main__':
     # bot.polling()
-    import time
-    bot.remove_webhook()
-    time.sleep(1)
-    bot.set_webhook(
-        config.WEBHOOK_URL,
-        certificate=open('webhook_cert.pem', 'r')
-    )
-    # app.run(debug=True)
+    # import time
+    # bot.remove_webhook()
+    # time.sleep(1)
+    # bot.set_webhook(
+    #     config.WEBHOOK_URL,
+    #     certificate=open('webhook_cert.pem', 'r')
+    # )
+    app.run(debug=True)
